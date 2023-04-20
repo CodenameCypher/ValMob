5488778e193b977563e6ec3086c3bcdff9ea653f
Aditya Roy <54760764+CodenameCypher@users.noreply.github.com>
2023-04-20 22:22:55 +0600

    Added flags api and Marquee Slide Text along with some fonts

--- lib/screens/upcomingSchedule/upcomingScheduleCard.dart
+++ lib/screens/upcomingSchedule/upcomingScheduleCard.dart
@@ -1,12 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:marquee/marquee.dart';
 import 'package:valmob/models/matches.dart' as model;
 import 'package:valmob/shared/theme.dart' as shared;
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:valmob/services/teams.dart';
 import 'package:valmob/models/team.dart';
+import 'package:valmob/services/flags.dart';
 
 class UpcomingScheduleCard extends StatelessWidget {
   final model.Match match;
@@ -27,80 +28,185 @@ class UpcomingScheduleCard extends StatelessWidget {
           padding: EdgeInsets.fromLTRB(0,10,0,10),
           child: ListTile(
             isThreeLine: true,
-            title: FutureBuilder(
-              future: Teams().getTeam(this.match.team_one_name, this.match.team_two_name),
-              builder: (context, t){
-                List<Team>? snapshot = t.data;
-                // print(snapshot![0].logo);
-                return Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                        child: Container(
-                          // color: Colors.blue,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                  padding: EdgeInsets.only(bottom: 7),
-                                child: snapshot != null && snapshot[0] != null && snapshot[0].logo != "" ? Image.network("https:"+snapshot[0].logo, height: 40, width: 40,) : Image.network("https://www.precisionpass.co.uk/wp-content/uploads/2018/03/default-team-logo.png", height: 40,width: 40),
-                              ),
-                              Text(
-                                this.match.team_one_name.replaceAll(" ", "\n"),
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 13
+            title: Column(
+              children: [
+                Text(
+                  this.match.event_name,
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 10
+                  ),
+                ),
+                FutureBuilder(
+                  future: Teams().getTeam(this.match.team_one_name, this.match.team_two_name),
+                  builder: (context, t){
+                    List<Team>? snapshot = t.data;
+                    return Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            margin: EdgeInsets.all(10),
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: shared.Theme.swatch2,
+                                borderRadius: BorderRadius.all(Radius.circular(10))
+                            ),
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Opacity(
+                                    opacity: 0.1,
+                                  child: Image.network(
+                                      Flag().getLink(this.match.flag1),
+                                    width: 73,
+                                  ),
                                 ),
-                              ),
-                            ],
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(bottom: 7),
+                                      child: snapshot != null && snapshot[0] != null && snapshot[0].logo != "" ? Image.network("https:"+snapshot[0].logo, height: 40, width: 40,) : Image.network("https://www.precisionpass.co.uk/wp-content/uploads/2018/03/default-team-logo.png", height: 40,width: 40),
+                                    ),
+                                    this.match.team_one_name.split(" ").length == 1 ? Text(
+                                      this.match.team_one_name,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          letterSpacing: 1,
+                                          fontFamily: "Font4"
+                                      ),
+                                    ) : SizedBox(
+                                      height: 20,
+                                      width: 70,
+                                      child: Marquee(
+                                        text: this.match.team_one_name,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            letterSpacing: 1,
+                                            fontFamily: "Font4"
+                                        ),
+                                        scrollAxis: Axis.horizontal,
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        blankSpace: 10.0,
+                                        velocity: 40.0,
+                                        accelerationDuration: Duration(seconds: 1),
+                                        accelerationCurve: Curves.linear,
+                                        decelerationDuration: Duration(milliseconds: 500),
+                                        decelerationCurve: Curves.easeOut,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            )
                           ),
                         ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                        child: Container(
-                          // color: Colors.red,
-                          child: Text(
-                            "V/S",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20
-                            ),
-                          ),
-                        )
-                    ),
-                    Expanded(
-                      flex: 1,
-                        child: Container(
-                          // color: Colors.blue,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                  padding: EdgeInsets.only(bottom: 7),
-                                child: snapshot != null && snapshot[1] != null && snapshot[1].logo != "" ?Image.network("https:"+snapshot[1].logo, height: 40,width: 40) : Image.network("https://www.precisionpass.co.uk/wp-content/uploads/2018/03/default-team-logo.png", height: 40,width: 40),
-                              ),
-                              Text(
-                                this.match.team_two_name.replaceAll(" ", "\n"),
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 13
+                        Expanded(
+                            flex: 1,
+                            child: Container(
+                              // color: Colors.red,
+                              child: CircleAvatar(
+                                child: Stack(
+                                  children: [
+                                    Opacity(
+                                        opacity: 0.2,
+                                      child: Image.network(
+                                        this.match.event_icon_url,
+                                        height: 40,
+                                      ),
+                                    ),
+                                    Text(
+                                        'V/S',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.w500,
+                                          fontFamily: 'Font3',
+                                      ),
+                                    )
+                                  ],
+                                  alignment: Alignment.center,
                                 ),
+                                backgroundColor: shared.Theme.swatch1Light,
+                              )
+                            )
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: Container(
+                              margin: EdgeInsets.all(10),
+                              padding: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  color: shared.Theme.swatch2,
+                                  borderRadius: BorderRadius.all(Radius.circular(10))
                               ),
-                            ],
-                          ),
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Opacity(
+                                    opacity: 0.1,
+                                    child: Image.network(
+                                      Flag().getLink(this.match.flag2),
+                                      width: 73,
+                                    ),
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(bottom: 7),
+                                        child: snapshot != null && snapshot[1] != null && snapshot[1].logo != "" ?Image.network("https:"+snapshot[1].logo, height: 40,width: 40) : Image.network("https://www.precisionpass.co.uk/wp-content/uploads/2018/03/default-team-logo.png", height: 40,width: 40),
+                                      ),
+                                      this.match.team_two_name.split(" ").length == 1 ? Text(
+                                        this.match.team_two_name,
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            letterSpacing: 1,
+                                            fontFamily: "Font4"
+                                        ),
+                                      ) : SizedBox(
+                                        height: 20,
+                                        width: 70,
+                                        child: Marquee(
+                                          text: this.match.team_two_name,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 16,
+                                              letterSpacing: 1,
+                                              fontFamily: "Font4"
+                                          ),
+                                          scrollAxis: Axis.horizontal,
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          blankSpace: 10.0,
+                                          velocity: 40.0,
+                                          accelerationDuration: Duration(seconds: 1),
+                                          accelerationCurve: Curves.linear,
+                                          decelerationDuration: Duration(milliseconds: 500),
+                                          decelerationCurve: Curves.easeOut,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ]
+                              ),
+                            )
                         )
-                    )
-                  ],
-                );
-              },
+                      ],
+                    );
+                  },
+                ),
+              ],
             ),
-
             subtitle: Column(
               children: [
                 this.match.eta == "LIVE" ?
@@ -125,8 +231,10 @@ class UpcomingScheduleCard extends StatelessWidget {
                           'LIVE',
                             textAlign: TextAlign.center,
                             style: TextStyle(
+                                letterSpacing: 3,
                                 color: Colors.white,
-                                fontWeight: FontWeight.bold
+                                fontWeight: FontWeight.normal,
+                                fontFamily: "Font2"
                             ),
                           ),
                           style: NeumorphicStyle(
--- python api test/api.py
+++ python api test/api.py
@@ -1,9 +1,15 @@
 import requests
 import json
 
-url1 = "https://victorious-bat-gloves.cyclic.app/api/matches/upcoming"
+url1 = "https://flagcdn.com/en/codes.json"
 
 response1 = requests.request("GET", url1)
 d = json.loads(response1.text)
 
-print(d)
+newDict = {}
+
+for key, value in d.items():
+    newDict[value] = key
+
+with open('output1.txt','w') as f:
+    f.write(str(newDict))
